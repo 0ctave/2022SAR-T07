fe3a369ccc084bd5690e6211bb02e55ed4649e9f
Keracles <keracles10@gmail.com>
2022-06-01 14:26:32 +0200

    lol2

--- course_files/session5/Session05v2/entersim/__init__.py
+++
@@ -1,5 +0,0 @@
-"""entersim."""
-import sys
-print("Colored output enabled")
-from IPython.core import ultratb
-sys.excepthook = ultratb.FormattedTB(mode='Verbose', color_scheme='Linux', call_pdb=False)
